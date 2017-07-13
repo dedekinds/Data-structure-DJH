f84ecba48686400954497f0803bb587d6222cd38
dedekinds <469676727@qq.com>
2017-07-13 23:02:04 +0800

    Add files via upload

--- 第五章_E1-1_先序遍历.py
+++ 第五章_E1-1_先序遍历.py
@@ -1,6 +1,6 @@
 邓俊辉-数据结构-30240184_05-E1-1 先序遍历
 
-
+____________________________________
 #二叉树的先序遍历(递归版)
 def preorder(tree,nodelist=None):
     if nodelist is None:
@@ -10,14 +10,14 @@ def preorder(tree,nodelist=None):
         preorder(tree.getLeftChild(),nodelist)
         preorder(tree.getRightChild(),nodelist)
     return nodelist
-
+____________________________________
 #二叉树的先序遍历(递归版-简易版)
 def traverse(tree):
     if tree:
         print(tree.getRootVal())
         traverse(tree.getLeftChild())
         traverse(tree.getRightChild())
-
+____________________________________
 #二叉树的先序遍历（迭代版）
 #需要用到栈
 def preorder(tree):
@@ -31,7 +31,7 @@ def preorder(tree):
             s.push(temp.getRightChild())
         if temp.getLeftChild():
             s.push(temp.getLeftChild())
-    
+_____________________________________  
 #二叉树的先序遍历（迭代可扩展版）
 #需要用到栈
 def visitAlongLeftBranch(tree,s):#不断地遍历左边，将右孩子推入栈中
